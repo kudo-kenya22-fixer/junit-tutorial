3d08d05bc036c9faf240ad4c3664d339a25f3cdb
Kenya <kudo.kenya22@fixer.co.jp>
2022-08-18 16:52:34 +0900

    ✨ インテリジェイのJUnitチュートリアル完了

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Add two numbers")
+    void あああ() {
+        assertEquals(4, Calculator.add(2, 2));
+    }
+
+    @Test
+    @DisplayName("Multiply two numbers")
+    void multiply() {
+        assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
+                () -> assertEquals(-4, Calculator.multiply(2, -2)),
+                () -> assertEquals(4, Calculator.multiply(-2, -2)),
+                () -> assertEquals(0, Calculator.multiply(1, 0)));
+    }
+
+    @Test
+    public void multiplyで乗算結果が取得できる() {
+        double expected = 11;
+        double actual = Calculator.multiply(3, 4);
+        assertEquals(expected, actual);
+    }
+}
